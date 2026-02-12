6638f6219918bebfd64e8a04fb60e374269123af
pacocho-10 <pacocho1971p.q@gmail.com>
2026-02-12 10:31:49 +0900

    chenged

--- mvp/backend/infer.py
+++ mvp/backend/infer.py
@@ -1,16 +1,7 @@
+from flask import Flask, request, jsonify
 from ultralytics import YOLO
-import os
+from PIL import Image
+import io
 
-model = YOLO("best.pt")
-img_path = "test.HEIC"
-output_dir = "output"
 
-# 出力フォルダ作成
-os.makedirs(output_dir, exist_ok=True)
-
-results = model(img_path)
-
-# 保存する時に拡張子付きパスを指定
-for i, r in enumerate(results):
-    save_path = os.path.join(output_dir, f"result_{i}.jpg")
-    r.save(save_path)  # または r.plot() と cv2.imwrite(save_path, r.plot())
+app = 
--- mvp/frontend/js/index.js
+++ mvp/frontend/js/index.js
@@ -1,3 +1,26 @@
-function send(x) {
-  console.log(x);
+function send() {
+  const input = document.getElementById('imageInput');
+  if(!input.files || input.files.length === 0) {
+    alert('画像を選んでください');
+    return;
+  }
+
+  const file = input.files[0];
+  const formData = new FormData();
+  formData.append('image', file);
+
+  fetch('http:..localhost:5000/predict', { //サーバーのURLに変更
+    method: 'POST',
+    body: formData
+  })
+
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    document.getElementById('result').innerText = JSON.stringify(data,null,2);
+  })
+  .catch(err => {
+    console.error(err);
+    alert('送信エラー')
+  });
 }
