31eb09854e1d81ef0996d01eef85c8b75b6afca5
pacocho-10 <pacocho1971p.q@gmail.com>
2026-02-14 10:12:39 +0900

    added firestorage file to upload some pics to the particular folder in storage

---
+++ firebase_demo/firestore/index.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "firebase/storage";
+
+// .env 読み込み
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+
+// 初期化（1回だけ）
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+// アップロード関数
+export async function uploadImage(file) {
+  const storageRef = ref(storage, `images/${file.name}`);
+  await uploadBytes(storageRef, file);
+  return await getDownloadURL(storageRef);
+}
